09b536f0dc4b14d2c8c9aae8b17e4e90e563546b
theovincent <49516344+theovincent@users.noreply.github.com>
2021-06-09 14:06:36 +0200

    Remove checks if the variables were set normally
    The checks are done in the actions.yml file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,6 @@
 
 set -e
 
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-  exit 1
-fi
-
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-  exit 1
-fi
-
-# Default to us-east-1 if AWS_REGION not set.
-if [ -z "$AWS_REGION" ]; then
-  AWS_REGION="us-east-1"
-fi
-
 # Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
 if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
